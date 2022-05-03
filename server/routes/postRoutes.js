3b6edb46dfc5f66dc42394ca29435358a077be16
RiegerJulio <riegerproducoes@gmail.com>
2022-05-03 13:33:10 -0300

    feat: routes and bugfixes

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,9 +1,9 @@
-// const postRoutes = require('express').Router();
-// const postController = require('../controllers/postController');
+const postRoutes = require('express').Router();
+const postController = require('../controllers/postController');
 
-// postRoutes.get('/', postController.getAllPosts);
-// postRoutes.get('/', (req, res) => {
-//   res.send('Funfando');
-// });
+postRoutes.get('/', postController.getAllPosts);
+postRoutes.get('/', (req, res) => {
+  res.send('Funfando');
+});
 
-// module.exports = postRoutes;
+module.exports = postRoutes;
