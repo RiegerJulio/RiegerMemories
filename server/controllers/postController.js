e5df144f110f56e4c46367f2a909f933f798fa82
RiegerJulio <riegerproducoes@gmail.com>
2022-05-04 19:10:11 -0300

    bugfix: solving a bug in delete and update dataflow and function in controllers. all reqs working now in back-end

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -23,14 +23,15 @@ const createPost = async (req, res) => {
 };
 
 const updatePost = async (req, res) => {
-  const { id: _id } = req.params;
-  const post = req.body;
+  const { id } = req.params;
+  const { name, title, message, location, file }= req.body;
+  const postParams = { name, title, message, location, file, _id: id }
 
   try {
-    if (!mongoose.Types.ObjectId.isValid(_id)) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
       res.status(404).json({ message: 'Post not found' });
     }
-    const updatedPost = await postServices.updatePost(_id, post);
+    const updatedPost = await postServices.updatePost(id, postParams);
     res.status(200).json({ message: updatedPost });
   } catch (e) {
     res.status(500).json({ message: 'Something went wrong' });
@@ -38,10 +39,10 @@ const updatePost = async (req, res) => {
 }
 
 const deletePost = async (req, res) => {
-  const { id: _id } = req.params;
+  const { id } = req.params;
 
   try {
-    if (!mongoose.Types.ObjectId.isValid(_id)) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
       res.status(404).json({ message: 'Post not found' });
     }
     const deletedPost = await postServices.deletePost(id)
