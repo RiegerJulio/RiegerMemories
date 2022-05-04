eb12ac6f3e9d088be2a3ad5559d0d27176caa5fb
RiegerJulio <riegerproducoes@gmail.com>
2022-05-04 15:48:00 -0300

    fix: fixing typo in constants reducer

--- client/src/redux/reducers/postsReducer.js
+++ client/src/redux/reducers/postsReducer.js
@@ -1,14 +1,14 @@
-import { CREATE, UPDATE, DELETE, FETCH_POSTS } from "../../constants";
+import { CREATE_POST, UPDATE_POST, DELETE_POST, FETCH_POSTS } from "../../constants";
 
 const postsReducer = (state = [], action) => {
   switch (action.type) {
     case FETCH_POSTS:
       return action.payload;
-    case CREATE:
+    case CREATE_POST:
       return [...state, action.payload];
-    case UPDATE:
+    case UPDATE_POST:
       return state.map((post) => (post._id === action.payload._id ? action.payload : post));
-    case DELETE:
+    case DELETE_POST:
       return state.filter((post) => post._id !== action.payload);
     default:
       return state;
